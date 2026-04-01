4afb4197303f5b453bade99acf5c1ba0b340851b
pinheirrr <lucassspinheir@gmail.com>
2026-04-01 20:38:20 -0300

    aula 3

---
+++ aula02b-operadores.py
@@ -0,0 +1,32 @@
+num1 = 5
+num2 = 2
+
+print(type(num1), type(num2))
+
+operacao = num1 + num2
+print(operacao, type(operacao))
+
+#OPERADORES DE ATRIBUIÇÃO
+num = 15
+print() # pular linha
+
+#OPERADORES RELACIONAIS
+# > < >= <= != ==
+
+print()
+print(6 != 3)
+
+idade = 21
+print(idade >= 21)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade, type(maior_idade))
+
+# STRINGS
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1.upper() ==nome2.upper())
